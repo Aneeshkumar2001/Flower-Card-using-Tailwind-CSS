9a35ad061ade88fedf7e9cd35857c7d88508e52f
Aneeshkumar2001 <aneeshkumarakm01@gmail.com>
2024-05-25 12:04:38 +0530

    flower

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Appbar from "./components/Appbar";
+import Filters from "./components/Filters";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
+import SeasonSpecials from "./components/SeasonSpecials";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Appbar/>
+      <Filters/>
+      <Items/>
+      <SeasonSpecials/>
+      <Footer/>
+    </>
   );
 }
 
